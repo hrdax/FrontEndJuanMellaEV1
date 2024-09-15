89badf2a4e72d4f11ab29a941be468325ae6253a
hrdax <juanpa_200018@hotmail.com>
2024-09-15 20:18:47 -0300

    header responsive + bootstrap

--- src/app/components/shared/header/header.component.ts
+++ src/app/components/shared/header/header.component.ts
@@ -1,13 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, CommonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
 
+  ngOnInit(): void {
+    const dropdownSubmenus = document.querySelectorAll('.dropdown-submenu > a');
+    
+    dropdownSubmenus.forEach((submenu) => {
+      submenu.addEventListener('click', function (e) {
+        e.preventDefault();
+        const nextEl = submenu.nextElementSibling;
+        if (nextEl) {
+          nextEl.classList.toggle('show');
+        }
+      });
+    });
+  }
 }
