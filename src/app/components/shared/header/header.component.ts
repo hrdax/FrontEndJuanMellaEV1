be6c10ac0a188b449a0a2f9c329ed3b548c26b0e
hrdax <juanpa_200018@hotmail.com>
2024-09-15 22:46:25 -0300

    fix

--- src/app/components/shared/header/header.component.ts
+++ src/app/components/shared/header/header.component.ts
@@ -9,19 +9,7 @@ import { CommonModule } from '@angular/common';
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent {
 
-  ngOnInit(): void {
-    const dropdownSubmenus = document.querySelectorAll('.dropdown-submenu > a');
-    
-    dropdownSubmenus.forEach((submenu) => {
-      submenu.addEventListener('click', function (e) {
-        e.preventDefault();
-        const nextEl = submenu.nextElementSibling;
-        if (nextEl) {
-          nextEl.classList.toggle('show');
-        }
-      });
-    });
-  }
+
 }
