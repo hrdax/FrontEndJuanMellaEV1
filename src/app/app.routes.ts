d24831641ab79a5ef5e4b3a3ee29a1942c28e4ed
hrdax <juanpa_200018@hotmail.com>
2024-09-18 18:38:16 -0300

    ruta componente relaciones publicas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { HomeComponent } from './components/home/home.component';
 import { ConsejoMunicipalComponent } from './components/consejo-municipal/consejo-municipal.component';
 import { SecretariaMunicipalComponent } from './components/secretaria-municipal/secretaria-municipal.component';
 import { AsesorJuridicoComponent } from './components/asesor-juridico/asesor-juridico.component';
+import { RelacionesPublicasComponent } from './components/relaciones-publicas/relaciones-publicas.component';
 
 
 export const routes: Routes = [
@@ -13,6 +14,10 @@ export const routes: Routes = [
     {
         path: 'concejoMunicipal',
         component: ConsejoMunicipalComponent,
+    },
+    {
+        path: 'relacionesPublicas',
+        component: RelacionesPublicasComponent,
     },
     {
         path: 'secretariaMunicipal',
