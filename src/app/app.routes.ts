34741ceb6b36b905befbd5b0066a4b1049e7436e
hrdax <juanpa_200018@hotmail.com>
2024-09-18 15:24:08 -0300

    ruta concejo municipal

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,15 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { ConsejoMunicipalComponent } from './components/consejo-municipal/consejo-municipal.component';
 
 
 export const routes: Routes = [
     {
         path: '',
         component: HomeComponent,
+    },
+    {
+        path: 'concejoMunicipal',
+        component: ConsejoMunicipalComponent,
     },
 ];
