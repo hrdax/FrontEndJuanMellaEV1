a161865300b5d8bba68d69ba2933d1ddb8347811
hrdax <juanpa_200018@hotmail.com>
2024-09-19 14:38:11 -0300

    componente registro social, estilos

---
+++ src/app/components/registro-social/registro-social.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { NgOptimizedImage, CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-registro-social',
+  standalone: true,
+  imports: [NgOptimizedImage, CommonModule],
+  templateUrl: './registro-social.component.html',
+  styleUrl: './registro-social.component.css'
+})
+export class RegistroSocialComponent {
+
+}
